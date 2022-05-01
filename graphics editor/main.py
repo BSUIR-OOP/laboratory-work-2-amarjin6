a19c36cb63de9f1f31010bcb8f846a15e8e64104
Alexander Marjin <amarjin6@gmail.com>
2022-05-01 20:07:54 +0300

    Add dependency inversion principle

--- graphics editor/main.py
+++ graphics editor/main.py
@@ -1,11 +1,56 @@
 from tkinter import *
-from turtle import RawTurtle, TurtleScreen
 from tkinter.ttk import *
-import line, rectangle, polygon, circle, ellipse
+from turtle import RawTurtle, TurtleScreen
+from abc import ABC, abstractmethod
+import circle
+import ellipse
+import line
+import polygon
+import rectangle
+
+
+class Drawable(ABC):
+    @abstractmethod
+    def forward(self, distance):
+        ...
+
+    @abstractmethod
+    def circle(self, radius, extent=None, steps=None):
+        ...
+
+    @abstractmethod
+    def right(self, angle):
+        ...
+
+    @abstractmethod
+    def left(self, angle):
+        ...
+
+
+class CustomTurtle(Drawable):
+    def __init__(self):
+        # Create turtle inside tk
+        can = Canvas(root, width=800, height=500, bg="black")  # a canvas what will turn into turtle screen
+        tsc = TurtleScreen(can)
+        tur = RawTurtle(tsc)
+        can.pack()
+        self.t = tur
+
+    def forward(self, distance):
+        self.t.forward(distance)
+
+    def circle(self, radius, extent=None, steps=None):
+        self.t.circle(radius)
+
+    def right(self, angle):
+        self.t.right(angle)
+
+    def left(self, angle):
+        self.t.left(angle)
 
 
 class Figures:
-    def __init__(self, figure, i, t):
+    def __init__(self, figure, i, t: Drawable):
         lst = []
         for j in range(i):
             print(f"Enter {j + 1} argument:")
@@ -24,50 +69,46 @@ root.iconbitmap('icon.ico')
 root.geometry('800x650')
 root.configure(bg='grey')
 
-# Create turtle inside tk
-can = Canvas(root, width=800, height=500, bg="black")  # a canvas what will turn into turtle screen
-tsc = TurtleScreen(can)
-tur = RawTurtle(tsc)
-can.pack()
-
 # Create entryText
-entryText1 = StringVar()
-entryText2 = StringVar()
+# entryText1 = StringVar()
+# entryText2 = StringVar()
+
+tur = CustomTurtle()
 
 
 def lines():
-    figure = Figures(line.Line, 1, tur)
-    tur.color('red')
+    figure = Figures(line.Line, 1, tur.t)
+    tur.t.color('red')
     figure.draw()
-    tur.setpos(0, 0)
+    tur.t.setpos(0, 0)
 
 
 def rectangles():
-    figure = Figures(rectangle.Rectangle, 2, tur)
-    tur.color('green')
+    figure = Figures(rectangle.Rectangle, 2, tur.t)
+    tur.t.color('green')
     figure.draw()
-    tur.setpos(0, 0)
+    tur.t.setpos(0, 0)
 
 
 def polygons():
-    figure = Figures(polygon.Polygon, 2, tur)
-    tur.color('blue')
+    figure = Figures(polygon.Polygon, 2, tur.t)
+    tur.t.color('blue')
     figure.draw()
-    tur.setpos(0, 0)
+    tur.t.setpos(0, 0)
 
 
 def circles():
-    figure = Figures(circle.Circle, 1, tur)
-    tur.color('purple')
+    figure = Figures(circle.Circle, 1, tur.t)
+    tur.t.color('purple')
     figure.draw()
-    tur.setpos(0, 0)
+    tur.t.setpos(0, 0)
 
 
 def ellipses():
-    figure = Figures(ellipse.Ellipse, 1, tur)
-    tur.color('orange')
+    figure = Figures(ellipse.Ellipse, 1, tur.t)
+    tur.t.color('orange')
     figure.draw()
-    tur.setpos(0, 0)
+    tur.t.setpos(0, 0)
 
 
 def main():
@@ -89,13 +130,13 @@ def main():
               background=[('active', 'blue')])
 
     # Create entry
-    entryText1.set("Param 1")
-    entryText2.set("Param 2")
-    width = Entry(root, font=('calibri', 24), width=14, textvariable=entryText1)
-    height = Entry(root, font=('calibri', 24), width=14, textvariable=entryText2)
-    width.pack()
-    height.pack()
-    var = lambda: print(entryText1.get())
+    # entryText1.set("Param 1")
+    # entryText2.set("Param 2")
+    # width = Entry(root, font=('calibri', 24), width=14, textvariable=entryText1)
+    # height = Entry(root, font=('calibri', 24), width=14, textvariable=entryText2)
+    # width.pack()
+    # height.pack()
+    # var = lambda: print(entryText1.get())
 
     # Create buttons
     buttons = {'Line': lines, 'Rectangle': rectangles, 'Circle': circles, 'Polygon': polygons, 'Ellipse': ellipses}
